49b878a9b10359a4297d43228fe29170e57e7594
korai <zaynulabdin313@gmail.com>
2019-05-26 04:51:53 +0500

    Task 1 completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    println!("Area of Circle {}",area(2));
+}
+
+fn area(radius: i64) -> f64 {
+        std::f64::consts::PI * (radius as f64 * radius as f64)
 }
