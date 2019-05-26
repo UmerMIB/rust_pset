c98d7a0792d994eebd6f018882bec2a5473c1bd7
korai <zaynulabdin313@gmail.com>
2019-05-26 05:12:49 +0500

    2 complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     println!("Hello, world!");
     println!("Area of Circle {}",area(2));
+
+    is_negative(0)
 }
 
 fn area(radius: i64) -> f64 {
         std::f64::consts::PI * (radius as f64 * radius as f64)
+}
+
+fn is_negative(num: i64) {
+
+     if num > 0 { println!("Positive Number"); } else if num < 0 { println!("Negative Number"); } else {println!("Zero Entered")};
+
 }
